ba1f73dd38bffaae99150c163ce3dc37be27eb51
qgk25 <qg3035276228@126.com>
2022-10-14 01:33:37 +0800

    add some resource

--- resource.h
+++ resource.h
@@ -4,14 +4,17 @@
 //
 #define IDD_DIALOG1                     101
 #define IDD_MAIN                        101
+#define IDB_PLAY                        106
+#define IDB_PNG1                        109
+#define IDB_LOCAL                       109
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_RESOURCE_VALUE        110
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
