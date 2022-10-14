23582204754ce9c053fc6a783dd4a0b957a0811d
qgk25 <qg3035276228@126.com>
2022-10-15 00:46:26 +0800

    finish button change

--- player.asm
+++ player.asm
@@ -7,21 +7,63 @@ include	player.inc
 
 .code
 start:
-	invoke	GetModuleHandle, NULL
+	invoke GetModuleHandle, NULL
 	mov	hInstance, eax
-	invoke DialogBoxParamA, hInstance,IDD_MAIN, 0, offset MainProc, 0
+	invoke DialogBoxParamA, hInstance,IDD_MAIN, 0, offset mainProc, 0
 	invoke ExitProcess, 0
 
-MainProc proc dialogHandle : DWORD, message : DWORD, wParam : DWORD, lParam : DWORD
+mainProc proc dialogHandle : dword, message : dword, wParam : dword, lParam : dword
 	local wc : WNDCLASSEX
 	mov eax, message
+
 	.if eax == WM_INITDIALOG
 		mov   wc.style, CS_HREDRAW or CS_VREDRAW or CS_DBLCLKS
 		invoke RegisterClassEx, addr wc
+		invoke dialogInit, dialogHandle
+	.elseif eax == WM_COMMAND
+		mov	eax, wParam
+		.if	eax == IDC_PLAY
+			invoke musicPlayControl, dialogHandle, playButtonState
+		.elseif eax == IDC_LOCAL
+			mov eax, 2
+		.endif
 	.elseif	eax == WM_CLOSE
 		invoke	EndDialog, dialogHandle, 0
 	.endif
 	xor eax, eax
 	ret
-MainProc endp
+mainProc endp
+
+dialogInit proc dialogHandle : dword
+	invoke playButtonControl, dialogHandle, _PAUSE
+	ret
+dialogInit endp
+
+musicPlayControl proc dialogHandle : dword, state : byte
+	.if state == _PAUSE
+		invoke playButtonControl, dialogHandle, _PLAY
+
+	.else
+		invoke playButtonControl, dialogHandle, _PAUSE
+
+	.endif
+
+	ret
+musicPlayControl endp
+
+playButtonControl proc dialogHandle : dword, state : byte
+	.if state == _PAUSE
+		mov eax, IDI_PAUSE
+		mov playButtonState, _PAUSE
+	.else
+		mov eax, IDI_PLAY
+		mov playButtonState, _PLAY
+	.endif
+
+	invoke LoadImage, hInstance, eax, IMAGE_ICON, ICON_WIDTH, ICON_HEIGHT, LR_DEFAULTCOLOR
+	invoke SendDlgItemMessage, dialogHandle, IDC_PLAY, BM_SETIMAGE, IMAGE_ICON, eax
+	
+	ret
+playButtonControl endp
+
 end start
--- resource.h
+++ resource.h
@@ -2,19 +2,21 @@
 // Microsoft Visual C++ 生成的包含文件。
 // 供 Resource.rc 使用
 //
-#define IDD_DIALOG1                     101
 #define IDD_MAIN                        101
-#define IDB_PLAY                        106
-#define IDB_PNG1                        109
-#define IDB_LOCAL                       109
+#define IDI_PLAY                        119
+#define IDI_LOCAL                       121
+#define IDI_PAUSE                       122
+#define IDC_PLAY                        1004
+#define IDC_LOCAL                       1005
+#define IDC_BUTTON2                     1006
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        110
+#define _APS_NEXT_RESOURCE_VALUE        123
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_CONTROL_VALUE         1007
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
