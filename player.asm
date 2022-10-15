617aa4072a018fee17aa7c0c4cdd35294e1ee222
rjw20020718 <rjw20020718@163.com>
2022-10-15 16:55:44 +0800

    basically finish ProgressBar

--- player.asm
+++ player.asm
@@ -28,6 +28,29 @@ mainProc proc dialogHandle : dword, message : dword, wParam : dword, lParam : dw
 			invoke DialogBoxParam, hInstance, IDD_LIST, 0, offset listProc, 0
 			mov eax, 2 ; TODO: select the file and manage relative data struct
 		.endif
+	; 定时器消息
+	.elseif eax == WM_TIMER					
+		.if playButtonState == _PLAY		; 播放状态需要重绘滑块
+			invoke changeProgressBar, dialogHandle		
+		.endif
+	;slider消息
+	.elseif eax == WM_HSCROLL
+		;获取发送消息的Slider的控件号并存在curSlider变量里
+		invoke GetDlgCtrlID, lParam
+		mov currentSlider, eax
+		mov ax, WORD PTR wParam			;获取消息类别
+		; 进度条消息
+		.if currentSlider == IDC_PROGRESS
+			.if ax == SB_ENDSCROLL					;滚动结束
+				mov isDraggingProgressBar, 0
+				;invoke SendDlgItemMessage, dialogHandle, IDC_SongMenu, LB_GETCURSEL, 0, 0	;获取被选中的下标
+				.if eax != -1				;当前有歌曲被选中，则发送mcisendstring命令调整进度
+					invoke changeTime, dialogHandle	
+				.endif
+			.elseif ax == SB_THUMBTRACK;滚动消息
+				mov isDraggingProgressBar, 1
+			.endif
+		.endif
 	.elseif	eax == WM_CLOSE
 		invoke	EndDialog, dialogHandle, 0
 	.endif
@@ -37,6 +60,9 @@ mainProc endp
 
 dialogInit proc dialogHandle : dword
 	invoke playButtonControl, dialogHandle, _PAUSE
+
+	;设置计时器，每0.5s发送一次计时器消息
+	invoke SetTimer, dialogHandle, 1, 500, NULL
 	ret
 dialogInit endp
 
@@ -86,4 +112,50 @@ listProc proc dialogHandle : dword, message : dword, wParam : dword, lParam : dw
 	xor eax, eax
 	ret
 listProc endp
+
+; 改变进度条位置
+changeProgressBar proc dialogHandle: dword
+	local temp: dword
+	.if playButtonState == _PLAY		;若当前为播放状态
+		invoke mciSendString, addr getPositionCommand, addr songPosition, 32, NULL;获取当前播放位置
+		invoke StrToInt, addr songPosition	;当前进度转成int存在eax里
+		;add eax, 1000
+		mov temp, eax
+		.if isDraggingProgressBar == 0	;若当前用户没在拖时间条那么实时更新进度条位置
+			invoke SendDlgItemMessage, dialogHandle, IDC_PROGRESS, TBM_SETPOS, 1, temp
+		.endif
+		invoke displayTime, dialogHandle, temp
+	.endif
+	ret
+changeProgressBar endp
+
+;文字显示进度
+displayTime proc dialogHandle: dword, currentPosition: dword
+	mov eax, currentPosition
+	mov edx, 0
+	div timeScale
+	
+	mov edx, 0
+	div timeScaleSec
+	mov timeMinutePosition, eax
+	mov timeSecondPosition, edx
+	invoke wsprintf, addr mediaCommand, addr timeShow, timeMinutePosition, timeSecondPosition, timeMinuteLength, timeSecondLength
+	invoke SendDlgItemMessage, dialogHandle, IDC_PROSHOW, WM_SETTEXT, 0, addr mediaCommand
+	ret
+displayTime endp
+
+; 根据进度条位置改变播放时间
+changeTime proc dialogHandle: dword
+	invoke SendDlgItemMessage, dialogHandle, IDC_PROGRESS, TBM_GETPOS, 0, 0		;TBM_GETPOS获取进度条位置到eax
+	invoke wsprintf, addr mediaCommand, addr setPositionCommand, eax
+	invoke mciSendString, addr mediaCommand, NULL, 0, NULL
+	.if playButtonState == _PLAY	
+		invoke mciSendString, addr playSongCommand, NULL, 0, NULL
+	.elseif playButtonState == _PAUSE
+		invoke mciSendString, addr playSongCommand, NULL, 0, NULL
+		mov playButtonState, _PLAY
+	.endif
+	ret
+changeTime endp
+
 end start
--- resource.h
+++ resource.h
@@ -6,12 +6,13 @@
 #define IDI_PLAY                        119
 #define IDI_LOCAL                       121
 #define IDI_PAUSE                       122
+#define IDI_PLAY1                       122
 #define IDD_LIST                        123
 #define IDC_PLAY                        1004
 #define IDC_LOCAL                       1005
 #define IDC_LIST1                       1007
-#define IDC_SLIDER1                     1008
 #define IDC_Progress                    1008
+#define IDC_PROSHOW                     1011
 
 // Next default values for new objects
 // 
@@ -19,7 +20,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        125
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1009
+#define _APS_NEXT_CONTROL_VALUE         1012
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
